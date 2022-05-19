140dc08458a9bf09dcd67380cd1f748543cea791
Asem Qaffaf <asem@qaffaf.com>
2022-05-19 22:14:11 +0300

    add interval

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import json
+import threading
 import uuid
 import time
 from click import echo
@@ -30,8 +31,12 @@ class EchoTaskSet(TaskSet):
         
         self.ws = ws
         
+        
+        
         def _receive():
             while True:
+            # e = threading.Event()
+            # while not e.wait(5):
                 res = ws.recv()
                 data = json.loads(res)
                 end_at = time.time()
@@ -41,8 +46,9 @@ class EchoTaskSet(TaskSet):
                     name='ws/getRoomRtpCapabilities',
                     response_time=response_time,
                     response_length=len(res),
-                )
-
+                )    
+        
+        
         gevent.spawn(_receive)
 
     def on_quit(self):
@@ -50,19 +56,21 @@ class EchoTaskSet(TaskSet):
 
     @task
     def get_room_rtp_capabilities(self):
-        start_at = time.time()
-        body = json.dumps({
-                        "ms_token": 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4IiwiaWF0IjoxNjUyOTgzMzI2LCJleHAiOjE2NTMwNjk3MjZ9.dL_SbBQjrTV0Xsf8G9AEOCS1f7txd2wvrkidnrNpBCsl4UuLIbC3lAwiihZITmkOHMv3rbaqXUs2FO6sXU-XwecRNF4UDcG92VG1-nLpTdwhWFh04Dhtbfpp0Aj9VUyXo8VIIt53JiuiOzM_pt0lxdJwOR4HYgwGi8wC6p4Rmz4XcipGyMiftc7V1GDb4NIm0GIRamkQb2y52JZtvYAmULDJQM2ctPhK6gs-q0U2eqnQcmgkDs73lOTOC4EHBH_7_mJRh4il77Oae0XRPSHIiCPGb36oWBB5C9448ygLIaYJ6D4JcWhuOo67A0kX6W0VdG4qNcqSUBpV8Z2TpQstDg',
-                        "action": "getRoomRtpCapabilities",
-                        'start_at': start_at
-                        })
-        self.ws.send(body)
-        events.request_success.fire(
-            request_type='WebSocket Sent',
-            name='ws/getRoomRtpCapabilities',
-            response_time=int((time.time() - start_at) * 1000000),
-            response_length=len(body),
-        )
+        e = threading.Event()
+        while not e.wait(5):
+            start_at = time.time()
+            body = json.dumps({
+                            "ms_token": 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4IiwiaWF0IjoxNjUyOTgzMzI2LCJleHAiOjE2NTMwNjk3MjZ9.dL_SbBQjrTV0Xsf8G9AEOCS1f7txd2wvrkidnrNpBCsl4UuLIbC3lAwiihZITmkOHMv3rbaqXUs2FO6sXU-XwecRNF4UDcG92VG1-nLpTdwhWFh04Dhtbfpp0Aj9VUyXo8VIIt53JiuiOzM_pt0lxdJwOR4HYgwGi8wC6p4Rmz4XcipGyMiftc7V1GDb4NIm0GIRamkQb2y52JZtvYAmULDJQM2ctPhK6gs-q0U2eqnQcmgkDs73lOTOC4EHBH_7_mJRh4il77Oae0XRPSHIiCPGb36oWBB5C9448ygLIaYJ6D4JcWhuOo67A0kX6W0VdG4qNcqSUBpV8Z2TpQstDg',
+                            "action": "getRoomRtpCapabilities",
+                            'start_at': start_at
+                            })
+            self.ws.send(body)
+            events.request_success.fire(
+                request_type='WebSocket Sent',
+                name='ws/getRoomRtpCapabilities',
+                response_time=int((time.time() - start_at) * 1000000),
+                response_length=len(body),
+            )
 
 class EchoLocust(HttpUser):
     tasks = [EchoTaskSet]
