46b152886d7e7ebc8a9088c0da584305c79b3ede
Asem Qaffaf <asem@qaffaf.com>
2022-05-19 18:16:24 +0300

    add creatroom

--- locustfile.py
+++ locustfile.py
@@ -1,24 +1,27 @@
 
 from locust import HttpUser, between, task
-
+import json
 
 class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
-
+    access_token = ''
     def on_start(self):
         r = self.client.post("/users/login", {
             "username": "goku3",
             "password": "1234"
         })
-        print(r.json())
-
+        accessToken = r.json()['accessToken']
+        self.access_token = accessToken
+        
     @task
-    def login(self):
-        # self.client.post("/users/login")
-        self.client.post("/users/login", {
-            "username": "goku3-----",
-            "password": "1234"
-        })
+    def creatroom(self):
+        self.client.post(
+            url="/rooms",
+            data= 'title=roomA&inviteUser=8%2C24',
+            auth=None,
+            headers={"Authorization": "Bearer " + self.access_token , 'Content-Type': 'application/x-www-form-urlencoded'},
+        )
+        
         
 
 #     # @task
